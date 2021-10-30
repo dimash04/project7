3cbdafc1539ebc47698dbb76182a2745d77e2797
Dimash <serikpaevdimash@gmail.com>
2021-10-31 01:17:57 +0600

    project7

---
+++ assets/main.js
@@ -0,0 +1,14 @@
+// window.addEventListener('scroll', function(){
+//     var header = document.querySelector('.start')
+
+//     let scroll = header.getBoundingClientRect().top
+//     console.log(scroll)
+
+//     if (scroll < 100) {
+//         header.classList.remove('show')
+//         header.classList.add('hide')
+//     } else {
+//         header.classList.add('show')
+//         header.classList.remove('hide')
+//     }
+// })
